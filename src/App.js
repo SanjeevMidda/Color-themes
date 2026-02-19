1a4aafe6e28a00032ccba2ee43e39adbfb93edeb
Sanjeev Midda <s.midda1@gmail.com>
2026-02-19 20:31:12 +0000

    Refactored colors array into an array of objects to include id for each UI element rendered

--- src/App.js
+++ src/App.js
@@ -3,7 +3,30 @@ import "./index.css";
 
 function App() {
   // colors
-  const colors = ["red", "blue", "green", "orange", "grey"];
+  // const colors = ["red", "blue", "green", "orange", "grey"];
+
+  const colors = [
+    {
+      id: 1,
+      color: "red",
+    },
+    {
+      id: 2,
+      color: "blue",
+    },
+    {
+      id: 3,
+      color: "green",
+    },
+    {
+      id: 4,
+      color: "orange",
+    },
+    {
+      id: 5,
+      color: "grey",
+    },
+  ];
 
   // state to hold color selected
   const [colorSelected, setColorSelected] = useState("white");
@@ -14,8 +37,9 @@ function App() {
         {colors.map((color) => {
           return (
             <button
-              style={{ backgroundColor: color }}
-              onClick={() => setColorSelected(color)}
+              id={color.id}
+              style={{ backgroundColor: color.color }}
+              onClick={() => setColorSelected(color.color)}
             ></button>
           );
         })}
