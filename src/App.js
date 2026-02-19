d7e8e70b04fc75ca92714ae7a5e483ee47b481b7
Sanjeev Midda <s.midda1@gmail.com>
2026-02-19 20:06:40 +0000

    created and stored colors for our themes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  // colors
+  const colors = ["red", "blue", "green", "orange", "grey"];
+  // state to hold color selected
+  // create background color for App
+
+  return <div className="App">.colorContainer</div>;
 }
 
 export default App;
