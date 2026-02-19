c2edfad1170689427539d75b69ae56d0801e8766
Sanjeev Midda <s.midda1@gmail.com>
2026-02-19 20:49:53 +0000

    Moved colors out of component to prevent re-rendering on each render

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
 import { useState } from "react";
 import "./index.css";
 
-function App() {
-  const colors = [
-    {
-      id: 1,
-      color: "red",
-    },
-    {
-      id: 2,
-      color: "blue",
-    },
-    {
-      id: 3,
-      color: "green",
-    },
-    {
-      id: 4,
-      color: "orange",
-    },
-    {
-      id: 5,
-      color: "grey",
-    },
-  ];
+const COLORS = [
+  {
+    id: 1,
+    color: "red",
+  },
+  {
+    id: 2,
+    color: "blue",
+  },
+  {
+    id: 3,
+    color: "green",
+  },
+  {
+    id: 4,
+    color: "orange",
+  },
+  {
+    id: 5,
+    color: "grey",
+  },
+];
 
-  // state to hold color selected
+function App() {
   const [selectedColor, setSelectedColor] = useState("white");
 
   return (
     <div className="App" style={{ backgroundColor: selectedColor }}>
       <div className="themeContainer">
-        {colors.map((color) => {
+        {COLORS.map((color) => {
           return (
             <button
               key={color.id}
